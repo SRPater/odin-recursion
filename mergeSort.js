4d2d898120dfcb80758146ef9bf11fd778782019
SRPater <srpater@gmail.com>
2026-02-09 12:13:44 +0100

    Finish merge sort

--- mergeSort.js
+++ mergeSort.js
@@ -1,5 +1,32 @@
-const mergeSort = function() {
-  console.log("Merge Sort function called!");
+const mergeSort = function(array) {
+  if (array.length <= 1) return array;
+
+  const halfLength = Math.floor(array.length / 2);
+
+  const left = mergeSort(array.slice(0, halfLength));
+  const right = mergeSort(array.slice(halfLength));
+
+  let indexLeft = 0;
+  let indexRight = 0;
+  const mergedArray = [];
+
+  while (indexLeft < left.length && indexRight < right.length) {
+    if (left[indexLeft] <= right[indexRight]) {
+      mergedArray.push(left[indexLeft]);
+      indexLeft++;
+    } else {
+      mergedArray.push(right[indexRight]);
+      indexRight++;
+    }
+  }
+
+  mergedArray.push(...left.slice(indexLeft), ...right.slice(indexRight));
+
+  return mergedArray;
 }
 
-mergeSort();
+console.log(mergeSort([]));
+console.log(mergeSort([73]));
+console.log(mergeSort([1, 2, 3, 4, 5]));
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]));
+console.log(mergeSort([105, 79, 100, 110]));
